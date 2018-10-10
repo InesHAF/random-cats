f4e90a7031ce0587a0ab0fa47b928364b1614c84
Ines Hafiani <ineshafiani@gmail.com>
2018-10-10 17:02:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ app = Flask(__name__)
 
 # list of cat images
 images = [
-    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr06/2013/4/6/9/anigif_enhanced-buzz-32501-1$
-    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr06/2013/4/6/9/anigif_enhanced-buzz-22919-1$
-    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr03/2013/4/6/9/anigif_enhanced-buzz-24011-1$
-    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr06/2013/4/6/9/anigif_enhanced-buzz-32505-1$
-    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr06/2013/4/6/9/anigif_enhanced-buzz-32585-1$
-    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr03/2013/4/6/9/anigif_enhanced-buzz-23997-1$
-    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr06/2013/4/6/9/anigif_enhanced-buzz-22675-1$
-    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr01/2013/4/6/9/anigif_enhanced-buzz-578-136$
-    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr06/2013/4/6/9/anigif_enhanced-buzz-22911-1$
-    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr06/2013/4/6/8/anigif_enhanced-buzz-22680-1$
+    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr06/2013/4/6/9/anigif_enhanced-buzz-32501-1365253458-0.gif",
+    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr06/2013/4/6/9/anigif_enhanced-buzz-22919-1365255114-2.gif",
+    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr03/2013/4/6/9/anigif_enhanced-buzz-24011-1365253511-0.gif",
+    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr06/2013/4/6/9/anigif_enhanced-buzz-32505-1365253605-4.gif",
+    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr06/2013/4/6/9/anigif_enhanced-buzz-32585-1365253559-0.gif",
+    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr03/2013/4/6/9/anigif_enhanced-buzz-23997-1365253763-3.gif",
+    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr06/2013/4/6/9/anigif_enhanced-buzz-22675-1365255333-0.gif",
+    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr01/2013/4/6/9/anigif_enhanced-buzz-578-1365255277-0.gif",
+    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr06/2013/4/6/9/anigif_enhanced-buzz-22911-1365255823-0.gif",
+    "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr06/2013/4/6/8/anigif_enhanced-buzz-22680-1365251927-5.gif"
 ]
 
 @app.route('/')
